9c88798c2f61324e826ca21bfb567ac11f7e3d72
Olagunju daniel <todak2000@gmail.com>
2018-05-01 12:36:27 +0100

    ejs first commit

---
+++ js/indexjs.js
@@ -0,0 +1,58 @@
+/*functions for excersice one*/
+function toggleExcercise1(){
+	var excercise1 =  document.getElementById('excercise1');
+	excercise1.classList.toggle("div-hide");
+}
+
+function toggleSolution1(){
+	alert("Are you sure you want to check the answer to this Challenge?")
+ $('#solutionOne').removeClass("div-hide");
+
+
+
+}
+function toggleClose1(){
+	alert("Are you sure you now know how to tackle this type of challenge?")
+	$('#solutionOne').addClass("div-hide");
+
+}
+
+/*functions for excersice two*/
+function toggleExcercise2(){
+	var excercise2 =  document.getElementById('excercise2');
+	excercise2.classList.toggle("div-hide");
+}
+
+function toggleSolution2(){
+	alert("Are you sure you want to check the answer to this Challenge?")
+ $('#solutionTwo').removeClass("div-hide");
+
+
+
+}
+function toggleClose2(){
+	alert("Are you sure you now know how to tackle this type of challenge?")
+	$('#solutionTwo').addClass("div-hide");
+
+}
+
+/*functions for excersice three*/
+function toggleExcercise3(){
+	var excercise3 =  document.getElementById('excercise3');
+	excercise3.classList.toggle("div-hide");
+}
+
+function toggleSolution3(){
+	alert("Are you sure you want to check the answer to this Challenge?")
+ $('#solutionThree').removeClass("div-hide");
+
+
+
+}
+function toggleClose3(){
+	alert("Are you sure you now know how to tackle this type of challenge?")
+	$('#solutionThree').addClass("div-hide");
+
+}
+
+
